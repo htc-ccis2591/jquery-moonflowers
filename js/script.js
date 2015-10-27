7aded447c9c08c469228cb460269acc96a29761e
Mypeterhub <petersrun@yahoo.com>
2015-10-27 14:01:20 -0500

    jQuery-moonflowers codes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
-var menu = document.getElementById("menu");
-var espresso = document.getElementById("espresso");
 
-var p = document.createElement("p");
-console.log(p)
-var msg = document.createTextNode("Click a menu item to view a picture.");
-p.appendChild(msg);
-menu.insertBefore(msg, espresso);
-
-var menuItems = menu.getElementsByClassName("menu-item");
-
+$(document).ready(function() {
+var $menu = $("#menu")
+//var menu = document.getElementById("menu");
+//var espresso = document.getElementById("espresso");
+var $espresso = $("#espresso");
+ 
+//var p = document.createElement("p");
+//console.log(p)
+//var msg = document.createTextNode("Click a menu item to view a picture.");
+//p.appendChild(msg);
+//menu.insertBefore(msg, espresso);
+    
+var $p = $("<p>").text("Click a menu item to view a picture.").insertBefore($espresso);
+     $("#menu").prepend($p);
+var menuItems = $(".menu-item");
+//var menuItems = menu.getElementsByClassName("menu-item");
+ 
 var item;
 for (var i = 0; i < menuItems.length; i++ ) {
     item = menuItems[i];
-
-    item.nextElementSibling.setAttribute("class", "hide");
+    
+ $(item).next().attr("class", "hide");  //item.nextElementSibling.setAttribute("class", "hide");
 
     // Attach event handler
+//Book page 227, Listing 10.2.7
+    //$(item).click (function () {
     item.onclick = function () {
+        
+imgSrc = $(this).next().attr("src");
+//imgSrc = this.nextElementSibling.getAttribute("src");
 
-        imgSrc = this.nextElementSibling.getAttribute("src");
-
-        document.getElementById("aside-image").setAttribute("src", imgSrc);
-        document.getElementById("aside-image").removeAttribute("class");
+        $("#aside-image").attr("src", imgSrc);
+        //document.getElementById("aside-image").setAttribute("src", imgSrc);
+        $("#aside-image").removeAttr("class");
+        //document.getElementById("aside-image").removeAttribute("class");
     }
 }   
+     
+});
