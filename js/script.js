2acc05a139efc4b290a5b773d106fdd0ddbc932b
Ryan Guse <rj2189guse@gmal.com>
2015-10-27 11:36:43 -0500

    First

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
-var menu = document.getElementById("menu");
-var espresso = document.getElementById("espresso");
-
-var p = document.createElement("p");
+$(document).ready(function() {
+    
+var menu = $("menu");
+console.log(menu)    
+var espresso = $("espresso");
+console.log(espresso)
+var p = $("p").add("p");
+//var p = document.createElement("p");    
 console.log(p)
-var msg = document.createTextNode("Click a menu item to view a picture.");
-p.appendChild(msg);
-menu.insertBefore(msg, espresso);
-
-var menuItems = menu.getElementsByClassName("menu-item");
-
+var msg = $("p.textMessage").text("Click a menu item to view a picture.");
+//var msg = document.createTextNode("Click a menu item to view a picture.");
+//p.appendChild(msg);
+//menu.insertBefore(msg, espresso);
+var menuItems = $("menuItems").find("menu-item");
+//var menuItems = menu.getElementsByClassName("menu-item");
+console.log(menuItems);
+    
 var item;
-for (var i = 0; i < menuItems.length; i++ ) {
-    item = menuItems[i];
-
-    item.nextElementSibling.setAttribute("class", "hide");
+for (var i = 0; i < $.length; i++ ) {
+    item = $[i];
+    
+    $("img").next().attr("class, hide");
+//    item.nextElementSibling.setAttribute("class", "hide");
+    
 
     // Attach event handler
-    item.onclick = function () {
+    var click = $("menu").click(function(){
+//    item.onclick = function () {
+    console.log(click)  
 
-        imgSrc = this.nextElementSibling.getAttribute("src");
+        imgSrc = $("menu-item").next().attr("src");
+//        imgSrc = this.nextElementSibling.getAttribute("src");
+       
 
         document.getElementById("aside-image").setAttribute("src", imgSrc);
         document.getElementById("aside-image").removeAttribute("class");
-    }
+    })
 }   
+});
