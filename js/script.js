fcefcad6d9247b01ad76dad671bb34bab2f2397e
Andrew Steinbach <andrew.steinbach@outlook.com>
2015-10-28 16:23:43 -0500

    jQuery Assignment
    Converted old JS code in the assignment to jQuery; performs same stuff.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+var $menu = $("#menu")
+var $espresso = $("#espresso");
+var $asideimage = $("#aside-image")
+
+ $(document).ready(function () {
+     var $p = $("<p>Click a menu item to view a picture.</p>");
+     var menuItems = $(".menu-item");
+     
+     $("#menu").prepend($p);
+     $(menuItems).siblings().addClass("hide");
+     $(menuItems).on("click", function(){ 
+     $asideimage.attr("src", $(this).siblings().attr("src"));
+     $($asideimage).removeClass("hide");
+     })
+}); 
+
+/* OLD JS CODE -------------------------------------------------------------
 var menu = document.getElementById("menu");
 var espresso = document.getElementById("espresso");
 
@@ -24,3 +41,4 @@ for (var i = 0; i < menuItems.length; i++ ) {
         document.getElementById("aside-image").removeAttribute("class");
     }
 }   
+*/
