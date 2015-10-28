86fe2448b352e0743ed0c54eeea65f2485633dfd
imdjsarge <rgallo001@my.hennepintech.edu>
2015-10-27 21:43:17 -0500

    pics not gone yet

--- js/script.js
+++ js/script.js
@@ -5,15 +5,20 @@
         var espresso = $("#espresso");
 
         $("<p>").text("Click a menu item to view a picture.").insertBefore(espresso);
+        
+        
+$(".menu-item").each(function() {
+ $(".menu-item").attr("hide");
+                    
 
-        $("menu-item").attr("class", "hide");
 
         // Attach event handler
         $("#aside-image").click(function () {
 
             $("imgSrc") = $(this).next().attr("src");
 
-            $("#aside-image").attr("src", imgSrc).removeAttr("class");
+            $("#aside-image").attr("src", imgSrc).removeAttr("hide");
+            });
         });
     });
 })();
