c4fa297465a15abef2099b65a4daea24f24f1cdd
Ryan Guse <rj2189guse@gmal.com>
2015-10-28 15:15:42 -0500

    New

--- js/script.js
+++ js/script.js
@@ -5,35 +5,45 @@ console.log(menu)
 var espresso = $("espresso");
 console.log(espresso)
 var p = $("p").add("p");
+var hideClass = $("img").addClass("hide");
+console.log(hideClass);
+var insertP = $('#espresso').before("Click a menu item to view a picture.").append("<p>");
+console.log(insertP);
+   
+//var txtNode = $("p").append(document.createTextNode("New Text");
+//console.log(txtNode)  
 //var p = document.createElement("p");    
 console.log(p)
-var msg = $("p.textMessage").text("Click a menu item to view a picture.");
+
 //var msg = document.createTextNode("Click a menu item to view a picture.");
-//p.appendChild(msg);
+//var append = $("p").append("msg");
+//console.log(append);    
+    //p.appendChild(msg);
 //menu.insertBefore(msg, espresso);
-var menuItems = $("menuItems").find("menu-item");
+var menuItems = $(".menu-item").find();
 //var menuItems = menu.getElementsByClassName("menu-item");
 console.log(menuItems);
     
 var item;
 for (var i = 0; i < $.length; i++ ) {
-    item = $[i];
+    item = [i];
     
-    $("img").next().attr("class, hide");
+//    $("img").next().attr("class, hide");
 //    item.nextElementSibling.setAttribute("class", "hide");
     
 
     // Attach event handler
-    var click = $("menu").click(function(){
+    var click = $(".menu-item").click(function(){
 //    item.onclick = function () {
     console.log(click)  
 
-        imgSrc = $("menu-item").next().attr("src");
-//        imgSrc = this.nextElementSibling.getAttribute("src");
+        var imgSrc = $("<img>.src");
+//      imgSrc = this.nextElementSibling.getAttribute("src");
+        console.log(imgSrc);
        
 
-        document.getElementById("aside-image").setAttribute("src", imgSrc);
-        document.getElementById("aside-image").removeAttribute("class");
+        $("#aside-image.src").attr("imgSrc");
+        $("#aside-image").removeAttr("class");
     })
 }   
 });
