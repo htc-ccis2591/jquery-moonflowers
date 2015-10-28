224eba9b5f9a8473497f69236be605c913abe6f5
imdjsarge <rgallo001@my.hennepintech.edu>
2015-10-28 17:33:21 -0500

    working like a charm

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 (function () {
     $(document).ready(function () {
 
-        var menu = $("#menu");
         var espresso = $("#espresso");
 
         $("<p>").text("Click a menu item to view a picture.").insertBefore(espresso);
@@ -9,12 +8,11 @@
         $("img").attr("class", "hide");
 
         // Attach event handler
-        $("img").click(function () {
+        $(".menu-item").click(function () {
 
-            $("img") = $(this).next().attr("src");
+            var imgSrc = $(this).next().attr("src");
 
-            $("img").attr("src", imgSrc).removeAttr("class", "hide");
-
+            $("#aside-image").attr("src", imgSrc).removeAttr("class", "hide")
         });
     });
 })();
