d6fa169698c94b0327ca2ae31094535879f4fba5
ggauchan001 <ggauchan001@users.noreply.github.com>
2015-11-04 20:33:49 -0600

    Short Version

--- js/script.js
+++ js/script.js
@@ -1,44 +1,48 @@
- $(document).ready(function () {
 
-     var $menu = $("#menu");
-     var $espresso = $("#espresso");
+////---Shorter Version-----/////
+$(document).ready(function () {
 
-     $("<p>").text("Click a menu item to view a picture.").insertBefore($espresso);
+    $("<p>Click a menu item to view a picture.</p>").insertBefore($("#espresso"));
 
-//     the line above is the same as the following;
+    var $menuItem = $(".menu-item");
+    $menuItem.next().attr("class", "hide");
+    $menuItem.click(function() {
+        imgSrc = $(this).next().attr("src");
+        $(this).fadeOut().fadeIn();
+        $("#aside-image").attr("src", imgSrc).removeAttr("class");
 
-//     var $p = $("<p>");
-//     $p.text("Click a menu item to view a picture.");
-//     $p.insertBefore($espresso);
+    });
+});
 
-     //$("h3").slideUp(2000).slideDown(2000);
-     var $menuItem =$(".menu-item");
-     $menuItem.each(function(index,item){
-         //.slideDown(2000).slideUp(1000);
-        
-         $(item).next().attr("class", "hide");
-     });
-     
-                                 
-     $(".menu-item").click(function(){
-         var $imgSrc = $(this).next().attr("src");
-         $(this).fadeOut().fadeIn();                  
-         $("#aside-image").attr("src",$imgSrc).removeAttr("class");
-     });
-       
-     
- });
-
-
-
-
-
-
-
-
+//
+/////////---My Code-----////
+// $(document).ready(function () {
+//
+//             var $menu = $("#menu");
+//             var $espresso = $("#espresso");
+//
+//             $("<p>").text("Click a menu item to view a picture.").insertBefore($espresso);
+//
+//             var $menuItem = $(".menu-item");
+//
+//             $menuItem.each(function (index, item) {
+//                 $(item).next().attr("class", "hide");
+//             });
+//
+//             $(".menu-item").click(function () {
+//                 var $imgSrc = $(this).next().attr("src");
+//                 $(this).fadeOut().fadeIn();
+//                 $("#aside-image").attr("src", $imgSrc).removeAttr("class");
+//             });
+//      });
+//
+//
+//
 
 
 
+
+//----JavaScript-------//////
 
 //var menu = document.getElementById("menu");
 //var espresso = document.getElementById("espresso");
