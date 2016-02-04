9054b6ee4e1d7f8340bda5f524062ed325fb5879
sfingerson001 <sfingerson001@hennepintech.edu>
2016-02-04 16:41:01 -0600

    created jquery version

--- js/script.js
+++ js/script.js
@@ -1,26 +1,60 @@
-var menu = document.getElementById("menu");
-var espresso = document.getElementById("espresso");
+//Add text under the “Menu” heading to indicate that you can click on an item to see an image. 
+$('h2').after('<p> Click A Menu Item Below For Image. </p>');
 
-var p = document.createElement("p");
-console.log(p)
-var msg = document.createTextNode("Click a menu item to view a picture.");
-p.appendChild(msg);
-menu.insertBefore(msg, espresso);
 
-var menuItems = menu.getElementsByClassName("menu-item");
+//Hide images
+$('li').children('img').hide();
 
-var item;
-for (var i = 0; i < menuItems.length; i++ ) {
-    item = menuItems[i];
+//Then when an item is clicked, the corresponding image should show off to the side.
 
-    item.nextElementSibling.setAttribute("class", "hide");
+$ (function() {
+    var $menuItems = $('li');   
+    
+    $menuItems.on('click', function () {
+        
+        console.log(this);
+        
+        itemsrc = $(this).children('img').attr('src');
+        
+        $('#aside-image').attr('src',itemsrc);
+        
+    })
+    
+})
 
-    // Attach event handler
-    item.onclick = function () {
 
-        imgSrc = this.nextElementSibling.getAttribute("src");
 
-        document.getElementById("aside-image").setAttribute("src", imgSrc);
-        document.getElementById("aside-image").removeAttribute("class");
-    }
-}   
+
+
+//
+// JavaScript Method
+//
+
+/* 
+    var menu = document.getElementById("menu");
+    var espresso = document.getElementById("espresso");
+
+    var p = document.createElement("p");
+    console.log(p)
+    var msg = document.createTextNode("Click a menu item to view a picture.");
+    p.appendChild(msg);
+    menu.insertBefore(msg, espresso);
+
+    var menuItems = menu.getElementsByClassName("menu-item");
+
+    var item;
+    for (var i = 0; i < menuItems.length; i++ ) {
+        item = menuItems[i];
+
+        item.nextElementSibling.setAttribute("class", "hide");
+
+        // Attach event handler
+        item.onclick = function () {
+
+            imgSrc = this.nextElementSibling.getAttribute("src");
+
+            document.getElementById("aside-image").setAttribute("src", imgSrc);
+            document.getElementById("aside-image").removeAttribute("class");
+        }
+    }   
+*/
