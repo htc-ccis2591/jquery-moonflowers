8e2063206d97e18fb6da0c03546f1b34a6cd0f7d
David Gossell <itpro@gossellmn.com>
2016-02-06 17:18:47 -0600

    Hide images on load, show image for clicked menu item.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+$(function()
+{
+    //hide all the images except the aside image which isn't showing anyway
+    var asideImg = $('#aside-image');
+    $('img')
+        .not(asideImg)
+        .hide();
+    
+    // add "click an item" instruction
+    var hdr = $('h2');
+    hdr.after('<em>Click a menu item see a picture of your delicious selection.');
+    // create event handler for cliced menu item spans
+    $('span.menu-item').on('click', function()
+    {
+        var imgSource;
+        
+        // get the src  attribute for the img following the clicked span
+        imgSource = $(this).next().attr('src');
+        
+        // update the source for the aside image and get rid of it's "hide" class
+        // finally, show it cuz we hid it with all the other images.
+        $('#aside-image').attr('src', imgSource);
+        $('#aside-image').removeClass('hide');
+        
+        
+        console.log($('#aside-image').attr('src'));
+        
+    });
+})
 
 
 /*
