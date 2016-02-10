a47595ffce53c423016a648f16a66f203051dc40
caleb4716 <calebr47@gmail.com>
2016-02-09 21:11:37 -0600

    done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,16 @@
-var menu = document.getElementById("menu");
-var espresso = document.getElementById("espresso");
-
-var p = document.createElement("p");
-console.log(p)
-var msg = document.createTextNode("Click a menu item to view a picture.");
-p.appendChild(msg);
-menu.insertBefore(msg, espresso);
-
-var menuItems = menu.getElementsByClassName("menu-item");
-
-var item;
-for (var i = 0; i < menuItems.length; i++ ) {
-    item = menuItems[i];
-
-    // This next line no longer works 
-    // the .hide has been removed from the CSS file
-    item.nextElementSibling.setAttribute("class", "hide");
-
-    // Attach event handler
-    item.onclick = function () {
-
-        imgSrc = this.nextElementSibling.getAttribute("src");
-
-        document.getElementById("aside-image").setAttribute("src", imgSrc);
-        document.getElementById("aside-image").removeAttribute("class");
-    }
-}   
+$(document).ready(function() {
+	
+	//Hide all images except for aside image
+	$("img").hide();
+	$("#aside-image").show();
+	
+	//Add text
+	$("h2").after("<p>Click item to view image.</p>");
+	
+	//Click to show image
+	$(".menu-item").click(function(){
+		var source = $(this).next().attr("src");
+		$("#aside-image").attr("src", source);
+	});
+	
+});
