e4e157a6a6804a36443556eac72bac172692ec40
Hieu Pham <me4219fs@my.hennepintech.edu>
2016-09-21 17:12:07 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,9 @@
-var menu = document.getElementById("menu");
-var espresso = document.getElementById("espresso");
+$("<p>Click a menu item to view a picture.</p>").insertAfter('h2');
 
-var p = document.createElement("p");
-console.log(p)
-var msg = document.createTextNode("Click a menu item to view a picture.");
-p.appendChild(msg);
-menu.insertBefore(msg, espresso);
+$("#menu img").hide();
 
-var menuItems = menu.getElementsByClassName("menu-item");
-
-var item;
-for (var i = 0; i < menuItems.length; i++ ) {
-    item = menuItems[i];
-
-    // This next line no longer works 
-    // the .hide has been removed from the CSS file
-    item.nextElementSibling.setAttribute("class", "hide");
-
-    // Attach event handler
-    item.onclick = function () {
-
-        imgSrc = this.nextElementSibling.getAttribute("src");
-
-        document.getElementById("aside-image").setAttribute("src", imgSrc);
-        document.getElementById("aside-image").removeAttribute("class");
-    }
-}   
+$(".menu-item").on("click", function () {
+    var images = $(this).next().attr('src');
+    $("#aside-image").attr('src', images);
+    $('aside').removeClass("hide");
+});
