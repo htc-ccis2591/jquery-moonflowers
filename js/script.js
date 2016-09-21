60aad9ad6f6149ae0f3151973bbcdbc968876cb0
pyasger <pyasger@gmail.com>
2016-09-21 18:03:31 -0500

    done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,10 @@
-var menu = document.getElementById("menu");
-var espresso = document.getElementById("espresso");
-
-var p = document.createElement("p");
-console.log(p)
-var msg = document.createTextNode("Click a menu item to view a picture.");
-p.appendChild(msg);
-menu.insertBefore(msg, espresso);
-
-var menuItems = menu.getElementsByClassName("menu-item");
-
-var item;
-for (var i = 0; i < menuItems.length; i++ ) {
-    item = menuItems[i];
-
-    // This next line no longer works 
-    // the .hide has been removed from the CSS file
-    item.nextElementSibling.setAttribute("class", "hide");
-
-    // Attach event handler
-    item.onclick = function () {
-
-        imgSrc = this.nextElementSibling.getAttribute("src");
-
-        document.getElementById("aside-image").setAttribute("src", imgSrc);
-        document.getElementById("aside-image").removeAttribute("class");
-    }
-}   
+(function () {
+    $("img").hide();
+    var msg = "Click the menu to see the picture.";
+    $("h3#espresso").before(msg);
+    $(".menu-item").on("click", function () {
+        var imgSource = $(this).siblings().attr("src");
+        $("#aside-image").attr("src", imgSource).show();
+        //this shows the images after the click event
+    });
+}());
